100bef31164cf8e1ee72091b753cce68e9987b4d
Pawel <pawel1818@outlook.com>
2025-12-21 19:03:14 +0100

    zmiana komentarzy

--- api.php
+++ api.php
@@ -3,10 +3,14 @@
 /**
  * API.PHP - Backend dla importu faktur z KSeF
  * 
- * Endpointy:
- *   POST ?action=start_export  - Rozpoczyna import faktur
- *   GET  ?action=check_status  - Sprawdza status importu
- *   GET  ?action=download      - Pobiera plik ZIP
+ * UWAGA: Z perspektywy KSeF API jest to "export" (wysyłanie danych),
+ * ale z perspektywy aplikacji to "import" (pobieranie faktur).
+ * 
+ * Dostępne endpointy:
+ *   POST ?action=start_export  - Rozpoczyna export z KSeF API (import do aplikacji)
+ *   GET  ?action=check_status  - Sprawdza status exportu
+ *   GET  ?action=download      - Pobiera zaszyfrowany plik ZIP
+ * 
  */
 
 require_once __DIR__ . '/vendor/autoload.php';
@@ -137,7 +141,7 @@ try {
             // Autoryzacja
             $tokens = $ksef->authenticate($nip, $ksefToken);
             
-            // Import
+            // Export
             $exportResult = $ksef->startExport(
                 $tokens['accessToken'],
                 $subjectType,
